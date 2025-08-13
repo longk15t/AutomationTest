613252dd73c127407b8773b4ed79a020fe0bd3a0
Goldie Tran <goldie.tran@pepperstone.com>
2025-08-13 22:37:10 +0700

    Publish test report with new ctrf json file

--- playwright_web/playwright.config.ts
+++ playwright_web/playwright.config.ts
@@ -13,6 +13,19 @@ export default defineConfig({
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['json', { outputFile: 'test-results/results.json' }],
     ['junit', { outputFile: 'test-results/results.xml' }],
+    ['playwright-ctrf-json-reporter', {
+        screenshot: true,              // Optional: Include screenshots in the report. Defaults to 'false'.
+        testType: 'e2e',                // Optional: Specify the test type (e.g., 'api', 'e2e'). Defaults to 'e2e'.
+        appName: 'PlaywrightWeb',               // Optional: Specify the name of the application under test.
+        appVersion: '1.0.0',            // Optional: Specify the version of the application under test.
+        osPlatform: 'windows',            // Optional: Specify the OS platform.
+        osRelease: '11',             // Optional: Specify the OS release version.
+        buildUrl: "https://github.com",    // Optional: Specify the build url.
+        repositoryName: "AutomationTest",    // Optional: Specify the repository name.
+        repositoryUrl: "https://github.com/longk15t/AutomationTest", // Optional: Specify the repository url.
+        branchName: "master",             // Optional: Specify the branch name.
+        testEnvironment: "production"      // Optional: Specify the test environment (e.g. staging, production).
+    }]
   ],
   outputDir: 'test-results',
 
